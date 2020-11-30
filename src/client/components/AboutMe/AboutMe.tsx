6cb372bf52bdb2906efd62d86eff1d31af45d35f
KwehDev <KwehDev@protonmail.com>
2020-11-30 13:29:17 +0200

    Added Tech stack for FrontEnd

--- src/client/components/AboutMe/AboutMe.tsx
+++ src/client/components/AboutMe/AboutMe.tsx
@@ -1,5 +1,41 @@
-import { Box, Flex, Heading, Image, Link, Stack, Text } from '@chakra-ui/react'
+import {
+  Box,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  List,
+  ListIcon,
+  ListItem,
+  Stack,
+  StackDivider,
+  Text,
+} from '@chakra-ui/react'
+
+import {
+  SiCss3,
+  SiGatsby,
+  SiHtml5,
+  SiJavascript,
+  SiNextDotJs,
+  SiReact,
+  SiRedux,
+  SiTypescript,
+} from 'react-icons/si'
+
 import React from 'react'
+import {
+  ChakraIcon,
+  CSS3Icon,
+  GatsbyIcon,
+  HTML5Icon,
+  JavaScriptIcon,
+  NextJSIcon,
+  ReactIcon,
+  ReduxIcon,
+  TypeScriptIcon,
+} from '../TechIcon'
 
 export default function AboutMe() {
   return (
@@ -10,7 +46,9 @@ export default function AboutMe() {
       p={12}
       flexDir="column"
     >
-      <Heading pb={6}>Hi, I'm Tom</Heading>
+      <Heading pt={6} pb={8}>
+        Hi, I'm Tom
+      </Heading>
       <Flex>
         <Flex w="60%" px={4} flexDir="column" fontSize={20}>
           <Stack spacing={4}>
@@ -37,7 +75,7 @@ export default function AboutMe() {
             </Text>
           </Stack>
         </Flex>
-        <Flex w="40%" px={2} boxSize="lg">
+        <Flex w="40%" px={2} boxSize="md" ml="auto" mb="auto">
           <Box>
             <Image
               borderRadius="full"
@@ -46,6 +84,83 @@ export default function AboutMe() {
             />
           </Box>
         </Flex>
+      </Flex>
+      <Flex
+        bgColor="white"
+        mx="10%"
+        color="atom_dark.900"
+        py={12}
+        mt={4}
+        borderRadius="30px"
+      >
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          align="center"
+          w="100%"
+          px={8}
+          fontSize="1em"
+        >
+          <Stack
+            align="center"
+            textAlign="center"
+            flex="1 0"
+            direction="column"
+            spacing={8}
+            divider={<StackDivider />}
+          >
+            <Heading fontSize="1.5em">Front-End</Heading>
+            <Text>
+              I prioritize accessible, clean UI. I typically write my own CSS,
+              using Chakra-UI for reusability and efficiency.
+            </Text>
+            <Heading fontSize="1.4em">Tech Stack</Heading>
+            <List spacing={2} textAlign="initial">
+              <ListItem>
+                <ListIcon as={HTML5Icon} />
+                HTML5
+              </ListItem>
+              <ListItem>
+                <ListIcon as={CSS3Icon} />
+                CSS3
+              </ListItem>
+              <ListItem>
+                <ListIcon as={JavaScriptIcon} />
+                JavaScript
+              </ListItem>
+              <ListItem>
+                <ListIcon as={TypeScriptIcon} />
+                TypeScript
+              </ListItem>
+              <ListItem>
+                <ListIcon as={ReactIcon} />
+                React
+              </ListItem>
+              <ListItem>
+                <ListIcon as={ReduxIcon} />
+                Redux
+              </ListItem>
+              <ListItem>
+                <ListIcon as={GatsbyIcon} />
+                Gatsby
+              </ListItem>
+              <ListItem>
+                <ListIcon as={NextJSIcon} />
+                Next.js
+              </ListItem>
+              <ListItem>
+                <ListIcon as={ChakraIcon} />
+                Chakra UI
+              </ListItem>
+            </List>
+          </Stack>
+
+          <Stack
+            flex="1 0"
+            direction="column"
+            divider={<StackDivider />}
+          ></Stack>
+        </Stack>
       </Flex>
     </Flex>
   )
